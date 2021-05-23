c4dc777a2bce97871cf2e341ee831f4a6e74639b
unknown <Alexhoropchanu@gmail.com>
2021-05-23 20:09:46 +0300

    finished dz

---
+++ src/App.vue
@@ -0,0 +1,70 @@
+<template>
+  <div id="app">
+    <header>
+      <h1 :class="$style.header">My personal costs</h1>
+    </header>
+    <main>
+      <button @click="clicked">ADD NEW COST+</button>
+      <PaymentForm @add="onDataAdded" v-show="isShowed" />
+      <PaymentsList :items="paymentsList" />
+    </main>
+  </div>
+</template>
+
+<script>
+import PaymentsList from "./components/PaymentsList";
+import PaymentForm from "./components/PaymentForm";
+
+export default {
+  name: "App",
+  components: { PaymentsList, PaymentForm },
+  data() {
+    return {
+      isShowed: false,
+      paymentsList: [
+        {
+          date: "13.05.2021",
+          category: "Education",
+          price: 123,
+        },
+        {
+          date: "12.05.2021",
+          category: "Education",
+          price: 456,
+        },
+        {
+          date: "11.05.2021",
+          category: "Education",
+          price: 789,
+        },
+        {
+          date: "10.05.2021",
+          category: "Education",
+          price: 110,
+        },
+      ],
+    };
+  },
+  methods: {
+    clicked() {
+      if (this.isShowed) {
+        this.isShowed = false;
+      } else {
+        this.isShowed = true;
+      }
+    },
+    onDataAdded(data) {
+      this.paymentsList.push(data);
+    },
+    show(isShowed) {
+      this.isShowed = isShowed;
+    },
+  },
+};
+</script>
+
+<style module>
+.header {
+  color: red;
+}
+</style>
---
+++ src/components/PaymentForm.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div>
+      <input placeholder="Date" v-model="date" />
+      <input placeholder="Category" v-model="category" />
+      <input placeholder="Price" v-model.number="price" />
+      <button @click="save">Save</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      date: "",
+      category: "",
+      price: 0,
+    };
+  },
+  props: {
+    items: Array,
+  },
+  methods: {
+    save() {
+      if (this.date && this.category) {
+        const { date, category, price } = this;
+        this.$emit("add", { date, category, price });
+        this.date = "";
+        this.category = "";
+        this.price = 0;
+      }
+    },
+  },
+};
+</script>
+
+<style></style>
