40f797aed4b1bcd3b3ff003f23a61bf72d8d3cd5
unknown <Alexhoropchanu@gmail.com>
2021-05-23 20:26:48 +0300

    Please help me with radio button

--- src/App.vue
+++ src/App.vue
@@ -1,70 +1,32 @@
 <template>
   <div id="app">
-    <header>
-      <h1 :class="$style.header">My personal costs</h1>
-    </header>
-    <main>
-      <button @click="clicked">ADD NEW COST+</button>
-      <PaymentForm @add="onDataAdded" v-show="isShowed" />
-      <PaymentsList :items="paymentsList" />
-    </main>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
   </div>
 </template>
 
-<script>
-import PaymentsList from "./components/PaymentsList";
-import PaymentForm from "./components/PaymentForm";
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
-export default {
-  name: "App",
-  components: { PaymentsList, PaymentForm },
-  data() {
-    return {
-      isShowed: false,
-      paymentsList: [
-        {
-          date: "13.05.2021",
-          category: "Education",
-          price: 123,
-        },
-        {
-          date: "12.05.2021",
-          category: "Education",
-          price: 456,
-        },
-        {
-          date: "11.05.2021",
-          category: "Education",
-          price: 789,
-        },
-        {
-          date: "10.05.2021",
-          category: "Education",
-          price: 110,
-        },
-      ],
-    };
-  },
-  methods: {
-    clicked() {
-      if (this.isShowed) {
-        this.isShowed = false;
-      } else {
-        this.isShowed = true;
-      }
-    },
-    onDataAdded(data) {
-      this.paymentsList.push(data);
-    },
-    show(isShowed) {
-      this.isShowed = isShowed;
-    },
-  },
-};
-</script>
+#nav {
+  padding: 30px;
 
-<style module>
-.header {
-  color: red;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
