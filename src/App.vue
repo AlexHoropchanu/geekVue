c92258e6a20514dd6cfe99d78f889615867fcbae
unknown <Alexhoropchanu@gmail.com>
2021-05-24 22:59:54 +0300

    finishedDZ

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,47 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <header>
+      <h1 :class="$style.header">My personal costs</h1>
+    </header>
+    <main>
+      <button @click="clicked">ADD NEW COST+</button>
+      <PaymentForm @add="onDataAdded" v-show="isShowed" />
+      <PaymentsList />
+    </main>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import PaymentsList from "./components/PaymentsList";
+import PaymentForm from "./components/PaymentForm";
+import { mapActions, mapState } from "vuex";
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+export default {
+  name: "App",
+  components: { PaymentsList, PaymentForm },
+  data() {
+    return {
+      paymentsList: []
+    };
+  },
+  computed: mapState(["isShowed"]),
+  methods: {
+    ...mapActions(["fetchData"]),
+    onDataAdded(data) {
+      this.$store.commit("setDataAdded", data);
+    },
+    clicked() {
+      this.$store.commit("setIsShowed");
     }
+  },
+  mounted() {
+    this.fetchData();
   }
+};
+</script>
+
+<style module>
+.header {
+  color: red;
 }
 </style>
---
+++ src/components/PaymentsList.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <ul
+      v-for="(item, index) in getPaymentsList"
+      :key="index"
+      :class="$style.list"
+    >
+      <li :class="$style.list__link">{{ index + 1 }}</li>
+      <li :class="$style.list__link">{{ item.date }}</li>
+      <li :class="$style.list__link">{{ item.category }}</li>
+      <li :class="$style.list__link">{{ item.price }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+export default {
+  props: {},
+  methods: {
+    doSomething() {
+      console.log(this.items);
+    }
+  },
+  computed: {
+    ...mapGetters(["getPaymentsList"])
+  }
+};
+</script>
+
+<style module>
+.list {
+  display: flex;
+  list-style-type: none;
+}
+.list__link {
+  margin-right: 100px;
+}
+</style>
